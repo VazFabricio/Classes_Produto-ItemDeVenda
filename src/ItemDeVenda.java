567c051ea97967108f260ffb0980bb6be591015f
fabricio vaz <fabricio.vaz2004@gmail.com>
2022-11-11 20:59:31 -0300

    Criada as classes Produto e ItemDeVenda, com seus respectivos atributos e seus get/set

---
+++ src/ItemDeVenda.java
@@ -0,0 +1,21 @@
+public class ItemDeVenda {
+     private Produto produto;
+     private int quantidade;
+     private double subtotal;
+
+     public ItemDeVenda(Produto produto, Produto preco) {
+
+     }
+
+     public Produto getProduto() {
+          return produto;
+     }
+
+     public int getQuantidade() {
+          return quantidade;
+     }
+
+     public double getSubtotal() {
+          return subtotal;
+     }
+}
---
+++ src/Produto.java
@@ -0,0 +1,87 @@
+public class Produto {
+
+    private String idProduto;
+    private String nome;
+    private String categoria;
+    private String marca;
+    private String modelo;
+    private double preco;
+    private double altura;
+    private double largura;
+    private double comprimento;
+    private double peso;
+
+    public Produto(){
+        idProduto = " ";
+        nome = " ";
+        categoria = " ";
+        marca = " ";
+        modelo = " ";
+        preco = 0.0;
+        altura = 0.0;
+        largura = 0.0;
+        comprimento = 0.0;
+        peso = 0.0;
+    }
+
+    public Produto(String idProduto, String nome, String categoria, double preco, double altura, double largura, double comprimento, double peso) {
+        this.idProduto = idProduto;
+        this.nome = nome;
+        this.categoria = categoria;
+        this.preco = preco;
+        this.altura = altura;
+        this.largura = largura;
+        this.comprimento = comprimento;
+        this.peso = peso;
+    }
+
+    public Produto(String idProduto, String nome, double preco, double altura, double largura, double comprimento, double peso) {
+        this.idProduto = idProduto;
+        this.nome = nome;
+        this.preco = preco;
+        this.altura = altura;
+        this.largura = largura;
+        this.comprimento = comprimento;
+        this.peso = peso;
+    }
+
+    public String getIdProduto() {
+        return idProduto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public double getComprimento() {
+        return comprimento;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+}
